f2eb3d515cc41aed7e87886aa897c613b497465b
like-rounak <rounaks.it.ug@jadavpuruniversity.in>
2023-08-06 09:51:23 +0530

    JS MASTERY added

---
+++ JS MASTERY TUTS/script.js
@@ -0,0 +1,32 @@
+// String
+// A data type used to represent text 
+
+console.log('String')
+
+const singleQuote= 'Test 1';
+const doubleQuote= "Test 2";
+
+// Simple Strings 
+
+console.log(singleQuote); 
+console.log(doubleQuote);
+
+const name = `Jane , ${singleQuote} `;  // Backtics ` `
+
+console.log(name);
+
+// To know the datatype
+console.log(typeof singleQuote);
+
+// NUmbers 
+// very straight forward 
+const a =5;
+const b= 10.55;
+
+const c = a-b;
+
+console.log (c);
+console.log (typeof c);
+
+//Booleans  - True (1) or False (0)
+
---
+++ JS MASTERY TUTS/variables.js
@@ -0,0 +1,19 @@
+// Comment in JS
+
+// Creating Variables
+var a='test';
+const b='test 2'; 
+
+// Other variables- let ; const
+// let - will be changes later 
+// const - Can't be changed Later
+
+/* 
+
+Multi Line Comment 
+
+*/
+
+console.log(a);
+console.log(b);
+
