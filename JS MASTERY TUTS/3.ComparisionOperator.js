d1ca43d1b722ea80b30ab28c17c2307aea4e2879
like-rounak <rounaks.it.ug@jadavpuruniversity.in>
2023-09-29 14:10:31 +0530

    Update

---
+++ JS MASTERY TUTS/3.ComparisionOperator.js
@@ -0,0 +1,17 @@
+//Comparision Operators 
+const a = 10;
+const b = 5;
+
+console.log(a >= b); 
+console.log(a <= b); 
+console.log(a != b); 
+console.log(a === b); // Strict equality
+console.log(a !== b); // Strict Inequality
+
+
+// Strict vs loose 
+
+console.log(0 == '0'); //true
+console.log(0 === '0'); //false
+console.log(true == "true"); //false
+console.log(true === "true");//false
